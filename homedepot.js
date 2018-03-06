7cd3cff3122f9f0b969b60c4f3b272157e692d4e
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2018-03-06 14:57:16 -0500

    fixes click events

--- homedepot.js
+++ homedepot.js
@@ -1,15 +1,64 @@
 var background = document.getElementsByClassName('container');
+var slideIndex = 1;
+var slides = document.getElementsByClassName('slides');
 
 function init() {
-  handleEventListeners();
+  handleArrows();
+  showSlides(slideIndex);
+  background[0].addEventListener('click', function() {
+    ADTECH.click('Banner', 'http://www.homedepot.com');
+  });
 }
 
-function handleEventListeners() {
-  background[0].addEventListener('click', function() {
-    ADTECH.click(
-      'Banner',
-      'https://www.homedepot.com/b/Appliances/Samsung/N-5yc1vZbv1wZa0f'
-    );
+function handleClick() {
+  if (slideIndex === 1) {
+    slides[slideIndex - 1].addEventListener('click', function(e) {
+      e.stopPropagation();
+      ADTECH.click(
+        '35 Percent Off',
+        'https://www.homedepot.com/b/Appliances/Samsung/N-5yc1vZbv1wZa0f'
+      );
+    });
+  } else if (slideIndex === 2) {
+    slides[slideIndex - 1].addEventListener('click', function(e) {
+      e.stopPropagation();
+      ADTECH.click(
+        'Spring',
+        'https://www.homedepot.com/b/Featured-Products-Appliances-Samsung-Spring-for-Something-New/N-5yc1vZcjlt'
+      );
+    });
+  }
+}
+
+function handleArrows() {
+  var leftArrow = document.getElementById('arrow-left');
+  var rightArrow = document.getElementById('arrow-right');
+  leftArrow.addEventListener('click', function(e) {
+    e.stopPropagation();
+    plusSlides(-1);
   });
+  rightArrow.addEventListener('click', function(e) {
+    e.stopPropagation();
+    plusSlides(1);
+  });
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  handleClick();
 }
 ADTECH.ready(init);
